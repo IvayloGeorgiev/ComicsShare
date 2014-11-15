68b6f0d58b313a67d3b17dae22837e3b327937fc
Ivaylo Georgiev <georgievir@yahoo.com>
2014-11-15 08:35:28 +0000

    Initial commit after project creation.

---
+++ ComicsShare/Web/ComicsShare.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComicsShare.Web.Startup))]
+namespace ComicsShare.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
