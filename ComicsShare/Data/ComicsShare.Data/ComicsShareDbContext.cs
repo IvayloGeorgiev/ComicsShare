9b9a2dffd1f81141fdb6ffabcdad7b4f5f557487
Ivaylo Georgiev <georgievir@yahoo.com>
2014-11-15 19:31:08 +0000

    Updated libraires.

---
+++ ComicsShare/Data/ComicsShare.Data/ComicsShareDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace ComicsShare.Data
+{    
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using ComicsShare.Data.Models;
+
+    public class ComicsShareDbContext : IdentityDbContext<User>
+    {
+        public ComicsShareDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ComicsShareDbContext Create()
+        {
+            return new ComicsShareDbContext();
+        }
+    }
+}
